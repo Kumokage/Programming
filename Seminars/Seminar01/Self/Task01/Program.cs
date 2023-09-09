a70a97c3422030dd89eb19256d31a4c8bc047739
ZhuravlevAlexey <leha.somik.zhuravlev@mail.ru>
2023-09-09 14:52:54 +0300

    Solution Task01

--- Seminars/Seminar01/Self/Task01/Program.cs
+++ Seminars/Seminar01/Self/Task01/Program.cs
@@ -1,1 +1,6 @@
 ﻿// Your code here!
+string name = Console.ReadLine();
+string secondName = Console.ReadLine();
+string middleName = Console.ReadLine();
+
+Console.Write("Имя: " + name + '\n' + "Фамилия: " + secondName + '\n' + "Отчество: " + middleName);
